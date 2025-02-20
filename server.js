b97a154288408db29e0aec2047e8ab8376def888
Alena <alena.kozlova2004@mail.ru>
2025-02-20 20:04:59 +0300

    Complete server

--- server.js
+++ server.js
@@ -8,24 +8,23 @@ app.use(express.json());
 app.use(express.static('public'));
 
 
-app.get('/tasks', async function (req, res) {
-    const client = new MongoClient('mongodb://127.0.0.1');
+async function getDbCollection(dbAddress, dbName, collectionName) {
+    const client = new MongoClient(dbAddress);
     await client.connect();
+    const db = client.db(dbName);
 
-    const db = client.db('todo_app');
-    const collection = db.collection('tasks');
+    return db.collection(collectionName);
+}
 
+app.get('/tasks', async function (req, res) {
+    const collection = await getDbCollection('mongodb://127.0.0.1', 'todo_app', 'tasks');
     const data = await collection.find({}).toArray();
 
     res.send(data);
 });
 
 app.get('/tasks/:id', async function (req, res) {
-    const client = new MongoClient('mongodb://127.0.0.1');
-    await client.connect();
-
-    const db = client.db('todo_app');
-    const collection = db.collection('tasks');
+    const collection = await getDbCollection('mongodb://127.0.0.1', 'todo_app', 'tasks');
 
     const data = await collection.findOne({ '_id': new ObjectId(req.params.id) });
 
@@ -33,42 +32,29 @@ app.get('/tasks/:id', async function (req, res) {
 });
 
 app.post('/tasks', async function (req, res) {
-    const client = new MongoClient('mongodb://127.0.0.1');
-    await client.connect();
-
-    const db = client.db('todo_app');
-    const collection = db.collection('tasks');
-
+    const collection = await getDbCollection('mongodb://127.0.0.1', 'todo_app', 'tasks');
     const task = { ...req.body, done: false };
 
     await collection.insertOne(task);
 
-    res.status(201).send();
+    res.status(201).send(task);
 });
 
 app.delete('/tasks/:id', async function (req, res) {
-    const client = new MongoClient('mongodb://127.0.0.1');
-    await client.connect();
-
-    const db = client.db('todo_app');
-    const collection = db.collection('tasks');
+    const collection = await getDbCollection('mongodb://127.0.0.1', 'todo_app', 'tasks');
 
     await collection.deleteOne({ '_id': new ObjectId(req.params.id) });
 
-    res.status(204).send();
+    res.status(204).send({});
 });
 
 app.patch('/tasks/:id', async function (req, res) {
-    const client = new MongoClient('mongodb://127.0.0.1');
-    await client.connect();
-
-    const db = client.db('todo_app');
-    const collection = db.collection('tasks');
+    const collection = await getDbCollection('mongodb://127.0.0.1', 'todo_app', 'tasks');
 
     await collection.updateOne({ '_id': new ObjectId(req.params.id) },
         { '$set': req.body });
 
-    res.status(200).send();
+    res.status(200).send({});
 });
 
 
