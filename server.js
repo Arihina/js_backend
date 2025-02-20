fa13a3e3cecd620c4bacc05152e09a48bd22ad3a
Alena <alena.kozlova2004@mail.ru>
2025-02-20 18:31:26 +0300

    add connection to db

--- server.js
+++ server.js
@@ -1,32 +1,37 @@
 const express = require('express');
-const app = express();
+const {MongoClient} = require('mongodb');
 
 const port = 8000;
+const app = express();
 
 app.use(express.static('public'))
 
 
-app.get('/about', function (req, res) {
-    const data = {
-        'app': {
-            'name': 'express js server',
-            'version': '0.0.1',
-            'tag': '#',
-            'about': 'nothing interesting yet'
-        }
-    }
+app.get('/tasks', function (req, res) {
+    const client = new MongoClient('mongodb://127.0.0.1');
+
+    const db = client.db('todo_app');
+    const collection = db.collection('tasks');
+    
+    const data = collection.find({}).toArray();
 
     res.send(data);
 });
 
-app.get('/route', function (req, res) {
-    let data = {};
+app.get('/tasks/:id', function (req, res) {
 
-    for (item in req.query) {
-        data[item] = 0.5 * Number(req.query[item]);
-    }
+});
 
-    res.send(data);
+app.post('/tasks', function (req, res) {
+    
+});
+
+app.delete('/tasks/:id', function (req, res) {
+
+});
+
+app.patch('/tasks/:id', function (req, res) {
+
 });
 
 
